acf01a5127de44df59dbdcb013a0a0f7f0e5229d
Antonio Moreno <amorenolopezbarajas@gmail.com>
2024-05-30 19:09:33 +0200

    Fixed

--- src/layout.sh
+++ src/layout.sh
@@ -237,8 +237,74 @@ reload_layouts() {
 
 # () -> ()
 select_layout_with_rofi() {
-  $($LAYOUTS/rofi_layout.sh) | exit 0
-}
+  # Determine the directory of the executable
+  DIR="$LAYOUTS"
+
+  ICON_DIR="$DIR/images"
+  ROFI_CONFIG="$DIR/config-rofi/config.rasi"
+  # Opciones de layout con iconos (usando el formato de iconos de rofi)
+  
+  IMAGES=("tall:$ICON_DIR/tall.jpeg"
+           "rtall:$ICON_DIR/rtall.jpeg"
+           "wide:$ICON_DIR/wide.jpeg"
+           "rwide:$ICON_DIR/rwide.jpeg"
+           "grid:$ICON_DIR/grid.jpeg"
+           "rgrid:$ICON_DIR/rgrid.jpeg"
+           "even:$ICON_DIR/even.jpeg"
+           "tiled:$ICON_DIR/tiled.jpeg"
+           "monocle:$ICON_DIR/monocle.jpeg")
+
+
+  # Crear el menú de opciones para rofi
+  MENU=""
+  for LAYOUT in "${IMAGES[@]}"; do
+      NAME=$(echo $LAYOUT | cut -d':' -f1)
+      ICON=$(echo $LAYOUT | cut -d':' -f2)
+      MENU="${MENU}${NAME}\x00icon\x1f${ICON}\n"
+  done
+
+  # Selección de layout usando rofi
+  SELECTED_LAYOUT=$(echo -e "$MENU" | rofi -dmenu -theme "$ROFI_CONFIG" -p "Selecciona un layout" -format "i s" -no-lazy-grab | awk '{print $2}')
+
+  # Cambiar el layout en bspwm
+  case $SELECTED_LAYOUT in
+      tall)
+          bsp-layout set tall
+          ;;
+      rtall)
+          bsp-layout set rtall
+          ;;
+      wide)
+          bsp-layout set wide
+          ;;
+      rwide)
+          bsp-layout set rwide
+          ;;
+      grid)
+          bsp-layout set grid
+          ;;
+      rgrid)
+          bsp-layout set rgrid
+          ;;
+      even)
+          bsp-layout set even
+          ;;
+      tiled)
+          bsp-layout set tiled
+          ;;
+      monocle)
+          bsp-layout set monocle
+          ;;
+      *)
+          echo "Layout no válido" 
+          ;;
+  esac
+
+  # Cerrar el programa después de aplicar el layout
+  exit 0
+
+} > /dev/null
+
 
 
 
